c5888500f9c02065c888b5864a45b4f49cd14b58
MutyalaSubbarayudu <mutyalasubbu95@gmail.com>
2020-07-29 12:33:40 +0530

    Add project files.

---
+++ Data/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class Employee : IdentityUser // refers to table - AspnetUsers
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+        public string TaxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+
+    }
+}
